d3c31c233210c7d31aef2a4cde62bae02f84ee25
QwazyWabbit <gandalf.2004@gmail.com>
2020-05-31 21:01:35 -0700

    Fixup FOFS macros.

--- g_local.h
+++ g_local.h
@@ -752,12 +752,14 @@ int	skullindex;
 extern	int	meansOfDeath;
 
 
-extern	edict_t			*g_edicts;
+#define q_offsetof(t, m)    ((size_t)&((t *)0)->m)
 
-#define	FOFS(x) (int)&(((edict_t *)0)->x)
-#define	STOFS(x) (int)&(((spawn_temp_t *)0)->x)
-#define	LLOFS(x) (int)&(((level_locals_t *)0)->x)
-#define	CLOFS(x) (int)&(((gclient_t *)0)->x)
+extern	edict_t* g_edicts;
+
+#define FOFS(x)     q_offsetof(edict_t, x)
+#define STOFS(x)    q_offsetof(spawn_temp_t, x)
+#define	LLOFS(x) (size_t)&(((level_locals_t *)0)->x)
+#define	CLOFS(x) (size_t)&(((gclient_t *)0)->x)
 
 //32 bit float precision caps this to 0.00000000023283064f which gives max result of .99999998407391880
 #define	random()	((randomMT() * 0.00000000023283064f))
